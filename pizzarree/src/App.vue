e5e83e20f76d311fd5413403fb598cf5fce26ca7
risha700 <ahbox@outlook.com>
2023-11-27 19:13:13 +1300

    bare quasar frontend

---
+++ pizzarree/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { TailwindDarkFix } from 'src/composables/tw-dark-fix';
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup(){
+    TailwindDarkFix();
+  }
+})
+</script>
---
+++ pizzarree/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      class="tw-w-8 tw-h-8"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
